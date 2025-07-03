d725d9d4388bba8095dad51e79034c4b6838c345
Battushig Namsraidorj <battushig@ema.gov.mn>
2025-07-03 14:50:20 +0800

    6. Custom Fonts in Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ConfigProvider } from "antd";
 import theme from "@/config/themeConfig";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { rubik } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={rubik.className}>
         <ConfigProvider theme={theme}>
           <AntdRegistry>
             <div className="main-container">
